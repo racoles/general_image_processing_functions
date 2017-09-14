f8fc0cded8a5f0d9c702b86e123865eacf51fd16
Coles <coles.81@asc.ohio-state.edu>
2017-09-14 13:38:15 -0400

    adding file readout for non-fits image process

--- colorAndIntensity.py
+++ colorAndIntensity.py
@@ -10,7 +10,8 @@ analyze the color or intensity of values in an image.
 
 Modules:
 colorFinder
-    This function converts a non-FITs type image to an array.
+    This function searches an array for a given RGBA color value above a given
+    threshold, and returns a list of all of the pixels that meet that criteria.
 '''
 
 # Import #######################################################################################
@@ -25,5 +26,6 @@ class colorAndIntensity(object):
             
     def colorFinder(self, colorArray, colorValue):
         '''
+        
         '''
         pass
