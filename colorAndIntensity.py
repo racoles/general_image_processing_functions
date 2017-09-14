5e17a761678324d9fc080db711b5bdf6fab5490d
Coles <coles.81@asc.ohio-state.edu>
2017-09-14 16:11:21 -0400

    adding file readout for non-fits image process

--- colorAndIntensity.py
+++ colorAndIntensity.py
@@ -24,8 +24,28 @@ class colorAndIntensity(object):
         Constructor
         '''
             
-    def colorFinder(self, colorArray, colorValue):
+    def colorFinder(self, colorArray, **kwargs):
         '''
-        
+        Search an array for a given RGBA color value above a given
+        threshold, and returns a list of all of the pixels that meet that criteria.
         '''
-        pass
+        #Initialize empty list that will be filled with pixels that fit the 
+            #users RGBA criteria.
+        colorCutPixels = []
+        #Search the data based on the colors that the user has specified
+        if kwargs is not None:
+            #Red
+            if 'r' in kwargs:
+                [colorCutPixels.append(colorArray[ii]) for ii in colorArray if colorArray[ii][0] >= kwargs.get('r')]
+            #Green
+            if kwargs is not None:
+                if 'g' in kwargs:
+                    [colorCutPixels.append(colorArray[jj]) for jj in colorArray if colorArray[jj][0] >= kwargs.get('g')]
+            #Blue
+            if kwargs is not None:
+                if 'b' in kwargs:
+                    [colorCutPixels.append(colorArray[kk]) for kk in colorArray if colorArray[kk][0] >= kwargs.get('b')]
+            #Alpha
+            if kwargs is not None:
+                if 'a' in kwargs:
+                    [colorCutPixels.append(colorArray[ll]) for ll in colorArray if colorArray[ll][0] >= kwargs.get('a')]
