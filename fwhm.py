1d9156c149128460a5d7dd3684f5b1bfc9cdc10f
Coles <coles.81@asc.ohio-state.edu>
2017-10-12 15:26:18 -0400

    added fwhm

---
+++ fwhm.py
@@ -0,0 +1,19 @@
+'''
+@title fwhm
+@author: Rebecca Coles
+Updated on Oct 12, 2017
+Created on Oct 12, 2017
+
+fwhm
+This module holds a series of functions that I use to find the
+full-well-half-maximum of a given curve
+
+Modules:
+fwhm2D
+    This function accepts a 2D array and returns the fwhm
+'''
+
+# Import #######################################################################################
+from fileHandling import fileHandling
+from datetime import date
+################################################################################################
