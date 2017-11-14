d12cbca5225023d7ba8fcfcc5aaafe3d1a7b4833
Coles <coles.81@asc.ohio-state.edu>
2017-11-14 15:39:22 -0500

    adding fwhm

--- fwhm.py
+++ fwhm.py
@@ -37,6 +37,6 @@ class fwhm(object):
         
         #find when function crosses line half_max (when sign of diff flips)
         #take the 'derivative' of signum(half_max - Y[])
-        d = yy - (max(yy) / 2) #sign(half_max - array(Y[0:-1])) - sign(half_max - array(Y[1:]))
-        indexes = where(d > 0)[0] 
+        dd = yy - (max(yy) / 2) #sign(half_max - array(Y[0:-1])) - sign(half_max - array(Y[1:]))
+        indexes = where(dd > 0)[0] 
         return abs(xx[indexes[-1]] - xx[indexes[0]])
